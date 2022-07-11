0cac25d74510177efbd8a478290d824d4001588d
drssth <drssth@gmail.com>
2022-07-11 15:34:42 +0200

    edit

---
+++ install.sh
@@ -0,0 +1,7 @@
+mamba install -c conda-forge pandas numpy scipy scikit-learn -y
+mamba install -c conda-forge opencv -y
+mamba install -c conda-forge h5py pyarrow simplejson -y
+mamba install -c conda-forge matplotlib seaborn -y
+mamba install -c conda-forge sqlalchemy tinydb -y
+mamba install -c conda-forge plotly dash -y
+mamba install -c conda-forge python-kaleido -y
